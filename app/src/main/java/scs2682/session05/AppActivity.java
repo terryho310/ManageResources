8037567c8d0fe7f49cdbf18988300fac33e32f63
Terry <terryho310@gmail.com>
2017-12-03 22:36:23 -0500

    Initial commit

---
+++ app/src/main/java/scs2682/session05/AppActivity.java
@@ -0,0 +1,13 @@
+package scs2682.session05;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.appactivity);
+    }
+}
